75fd94fbbb294ba8a1d90942a722be014a8e9fd4
venkatesh <vchary78@gmail.com>
2020-03-09 11:44:38 +0530

    adding the jwt authentication with motorclient mongodb access

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 # aiohttp_polls/main.py
-import logging
 from aiohttp import web
-from routes import setup_routes
+import motor.motor_asyncio
+from aiohttp_jwt import JWTMiddleware
+
+sharable_secret = "venkatesh"
+jwt_middleware = JWTMiddleware(
+    sharable_secret, request_property="user", credentials_required=False
+)
+
+
+async def setup():
+    client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
+    db = client['test_database']
+    print(db)
+    return db
 
 
 async def init_app():
-    app = web.Application(debug=True)
+    app = web.Application(middlewares=[jwt_middleware])
+    from routes import setup_routes
     setup_routes(app)
+    db = await setup()
+    app["db"] = db
+    app["secretkey"] = sharable_secret
     return app
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
     app = init_app()
     web.run_app(app)
 
--- views2.py
+++ views2.py
@@ -1,11 +1,73 @@
 from aiohttp import web
-from mylogger import LOG
-
+import bson
+from bson import ObjectId
+import datetime
+from aiohttp_jwt import login_required
+import jwt
+from .mylogger import LOG
+import json
 routes = web.RouteTableDef()
+
+
+def serialize(data):
+    d = {}
+    for i in data:
+        if isinstance(i["_id"], ObjectId):
+            str(i["_id"])
+
+
+class CustomEncoder(json.JSONEncoder):
+    """A C{json.JSONEncoder} subclass to encode documents that have fields of
+    type C{bson.objectid.ObjectId}, C{datetime.datetime}
+    """
+    def default(self, obj):
+        if isinstance(obj, bson.objectid.ObjectId):
+            return str(obj)
+        elif isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
 
 
 @routes.get("/")
 async def index(request):
     LOG.info("------ in the index api")
-    return web.Response(text='Hello Aiohttp!')
-    
+    return web.Response(text='Hello Aiohttp!', status=200)
+
+
+@routes.post("/register")
+async def registration(request):
+    LOG.info("request info at registration:{0}".format(dir(request)))
+    req_data = await request.json()
+    db = request.app["db"]
+    curs = await db.user.find_one({"username": req_data["username"]})
+    if curs is None:
+        inserted_id = await db.user.insert_one(await request.json())
+        return web.json_response({"message": "inserted", "id": str(inserted_id.inserted_id)}, status=200)
+    else:
+        return web.json_response({"message":"user already present"}, status=200)
+
+
+@routes.get("/users")
+@login_required
+async def get_users(request):
+    cursor = request.app["db"].user.find({})
+    list_users = []
+    enc = CustomEncoder()
+    for document in await cursor.to_list(length=100):
+        list_users.append(enc.encode(document))
+    return web.json_response({"userslist": list_users}, status=200)
+
+
+@routes.post("/login")
+async def do_login(request):
+    req_data = await request.json()
+    db = request.app["db"]
+    secret_key = request.app["secretkey"]
+    curs = await db.user.find_one({"username": req_data["username"], "password": req_data["password"]})
+    if curs is None:
+        return web.json_response({"message": "no user found"}, status=200)
+    else:
+        jwt_key = jwt.encode({"username": req_data["username"]}, secret_key).decode('UTF-8')
+    return web.json_response({"jwt_key": jwt_key}, status=200)
+
+
