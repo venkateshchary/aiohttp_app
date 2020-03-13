743def8d6c716373583c7ac7431d72293685cc19
VENKATESH <vchary78@gmail.com>
2020-03-13 08:14:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # aiohttp_polls/main.py
 from aiohttp import web
-<<<<<<< HEAD
 import motor.motor_asyncio
 from aiohttp_jwt import JWTMiddleware
 
@@ -14,26 +13,21 @@ async def setup():
     client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
     db = client['test_database']
     print(db)
-=======
 from routes import setup_routes
 import motor.motor_asyncio
 
 async def setup_db():
     client= motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
     db = client['test_database']
->>>>>>> 6df3bd7c92c13eff813ba0f861e090b10fcddfb8
     return db
 
 
 async def init_app():
-<<<<<<< HEAD
     app = web.Application(middlewares=[jwt_middleware])
     from routes import setup_routes
-=======
     app = web.Application(debug=True)
     db = await setup_db()
     app['db'] =db
->>>>>>> 6df3bd7c92c13eff813ba0f861e090b10fcddfb8
     setup_routes(app)
     db = await setup()
     app["db"] = db
